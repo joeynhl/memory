874cb9a37f0262a6dac4aeebc7448f72da454a9b
Erik van der Horst <erikvanderhorst@hotmail.com>
2019-10-01 23:29:25 +0200

    achtergrondafbeelding toegevoegd aan grid

--- Speelveld/MainWindow.xaml.cs
+++ Speelveld/MainWindow.xaml.cs
@@ -24,12 +24,41 @@ namespace Speelveld
         public MainWindow()
         {
             InitializeComponent();
+            setField();
 
-            Button btn = new Button();
-            btn.Content = "click me motherfucker";
 
-            grid.Children.Add(btn);
         }
+
         
+        private void setField()
+        {
+            string[,] cards = new string[4, 4]
+            {
+                {"1", "2", "3", "4"},
+                { "5", "6", "7", "8"},
+                { "9", "10", "11", "12"},
+                { "13", "14", "15", "16" }
+            };
+
+            for (int row = 0; row < cards.GetLength(0); row++)
+            {
+                for (int column = 0; column < cards.GetLength(1); column++)
+                {
+                    Button btn = new Button();
+                    btn.Content = cards[row, column];
+                    cardgrid.Children.Add(btn);
+                    btn.HorizontalContentAlignment = HorizontalAlignment.Stretch;
+                    btn.VerticalContentAlignment = VerticalAlignment.Stretch;
+                    btn.Margin = new Thickness(10);
+
+                    btn.SetValue(Grid.RowProperty, row);
+                    btn.SetValue(Grid.ColumnProperty, column);
+
+                    /* Grid.Row = "0" Grid.Column = "0"*/
+                }
+            }
+
+        }
+
     }
 }
