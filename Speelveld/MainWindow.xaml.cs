90374311ce4406fd8cd579afb3be0ece6efdcc1e
Erik van der Horst <erikvanderhorst@hotmail.com>
2019-10-07 21:40:18 +0200

    shuffle array

--- Speelveld/MainWindow.xaml.cs
+++ Speelveld/MainWindow.xaml.cs
@@ -25,25 +25,45 @@ namespace Speelveld
         {
             InitializeComponent();
             setField();
-
-
         }
 
-        
+        public static void Shuffle<T>(Random random, T[,] cards)
+        {
+            int lengthRow = cards.GetLength(1);
+
+            for (int i = cards.Length - 1; i > 0; i--)
+            {
+                int i0 = i / lengthRow;
+                int i1 = i % lengthRow;
+
+                int j = random.Next(i + 1);
+                int j0 = j / lengthRow;
+                int j1 = j % lengthRow;
+
+                T temp = cards[i0, i1];
+                cards[i0, i1] = cards[j0, j1];
+                cards[j0, j1] = temp;
+            }
+        }
+
         private void setField()
         {
-            string[,] cards = new string[4, 4]
+            string[,] cards = new string[2, 4]
             {
                 {"1", "2", "3", "4"},
-                { "5", "6", "7", "8"},
-                { "9", "10", "11", "12"},
-                { "13", "14", "15", "16" }
+                { "5", "6", "7", "8"}
             };
+            var rnd = new Random();
+
+            Shuffle(rnd, cards);
+
 
             for (int row = 0; row < cards.GetLength(0); row++)
             {
                 for (int column = 0; column < cards.GetLength(1); column++)
                 {
+
+
                     Button btn = new Button();
                     btn.Content = cards[row, column];
                     cardgrid.Children.Add(btn);
@@ -57,6 +77,7 @@ namespace Speelveld
                     /* Grid.Row = "0" Grid.Column = "0"*/
                 }
             }
+            
 
         }
 
