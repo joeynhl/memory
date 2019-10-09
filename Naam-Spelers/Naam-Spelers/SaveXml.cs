e94109057f3bffa4e719d5d9696df38ee2c5503f
Rienk <rienk2001@hotmail.com>
2019-10-09 12:13:44 +0200

    status

---
+++ Naam-Spelers/Naam-Spelers/SaveXml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace Naam_Spelers
+{
+    class SaveXml
+    {
+        public static void savedata(object obj, string filename)
+        {
+            XmlSerializer sr = new XmlSerializer(obj.GetType());
+            TextWriter writer = new StreamWriter(filename);
+            sr.Serialize(writer, obj);
+            writer.Close();
+        }
+    }
+}
