f4652eb2e79348c739fa0895aafc0e08daa12dfa
stylishpillow <43334036+stylishpillow@users.noreply.github.com>
2019-10-09 13:05:16 +0200

    changes

--- cards/cards/MainWindow.xaml.cs
+++ cards/cards/MainWindow.xaml.cs
@@ -27,59 +27,59 @@ namespace cards
         {
             InitializeComponent();
 
-            //Image rotateImage = new Image()
-            //{
-            //    Stretch = Stretch.Uniform,
-            //    Source = new BitmapImage(new Uri("pack://application:,,,/images/goodestboi.jpg")),
-            //    RenderTransform = new RotateTransform()
-            //};
-            //Image opacityImage = new Image()
-            //{
-            //    Stretch = Stretch.Uniform,
-            //    Source = new BitmapImage(new Uri("pack://application:,,,/images/sjekkie.jpg"))
-            //};
+            Image rotateImage = new Image()
+            {
+                Stretch = Stretch.Uniform,
+                Source = new BitmapImage(new Uri("pack://application:,,,/images/goodestboi.jpg")),
+                RenderTransform = new RotateTransform()
+            };
+            Image opacityImage = new Image()
+            {
+                Stretch = Stretch.Uniform,
+                Source = new BitmapImage(new Uri("pack://application:,,,/images/sjekkie.jpg"))
+            };
 
-            //LayoutRoot.Children.Add(rotateImage);
-            //LayoutRoot.Children.Add(opacityImage);
+            LayoutRoot.Children.Add(rotateImage);
+            LayoutRoot.Children.Add(opacityImage);
 
-            //Grid.SetColumn(opacityImage, 1);
+            Grid.SetColumn(opacityImage, 1);
 
-            //Storyboard storyboard = new Storyboard();
-            //storyboard.Duration = new Duration(TimeSpan.FromSeconds(3.0));
-            //DoubleAnimation rotateAnimation = new DoubleAnimation()
-            //{
-            //    From = 0,
-            //    To = 360,
-            //    Duration = storyboard.Duration
-            //};
-            //DoubleAnimation opacityAnimation = new DoubleAnimation()
-            //{
-            //    From = 1.0,
-            //    To = 0.0,
-            //    BeginTime = TimeSpan.FromSeconds(2.0),
-            //    Duration = new Duration(TimeSpan.FromSeconds(1.0))
-            //};
+            Storyboard storyboard = new Storyboard();
+            storyboard.Duration = new Duration(TimeSpan.FromSeconds(3.0));
+            DoubleAnimation rotateAnimation = new DoubleAnimation()
+            {
+                From = 0,
+                To = 360,
+                Duration = storyboard.Duration
+            };
+            DoubleAnimation opacityAnimation = new DoubleAnimation()
+            {
+                From = 1.0,
+                To = 0.0,
+                BeginTime = TimeSpan.FromSeconds(2.0),
+                Duration = new Duration(TimeSpan.FromSeconds(1.0))
+            };
 
-            //Storyboard.SetTarget(rotateAnimation, rotateImage);
-            //Storyboard.SetTargetProperty(rotateAnimation, new PropertyPath("(UIElement.RenderTransform).(RotateTransform.Angle)"));
-            //Storyboard.SetTarget(opacityAnimation, opacityImage);
-            //Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath("Opacity"));
+            Storyboard.SetTarget(rotateAnimation, rotateImage);
+            Storyboard.SetTargetProperty(rotateAnimation, new PropertyPath("(UIElement.RenderTransform).(RotateTransform.Angle)"));
+            Storyboard.SetTarget(opacityAnimation, opacityImage);
+            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath("Opacity"));
 
-            //storyboard.Children.Add(rotateAnimation);
-            //storyboard.Children.Add(opacityAnimation);
+            storyboard.Children.Add(rotateAnimation);
+            storyboard.Children.Add(opacityAnimation);
 
-            //Resources.Add("Storyboard", storyboard);
+            Resources.Add("Storyboard", storyboard);
 
-            //Button button = new Button()
-            //{
-            //    Content = "Begin"
-            //};
-            //button.Click += button_Click;
+            Button button = new Button()
+            {
+                Content = "Begin"
+            };
+            button.Click += button_Click;
 
-            //Grid.SetRow(button, 1);
-            //Grid.SetColumnSpan(button, 2);
+            Grid.SetRow(button, 1);
+            Grid.SetColumnSpan(button, 2);
 
-            //LayoutRoot.Children.Add(button);
+            LayoutRoot.Children.Add(button);
 
 
         }
