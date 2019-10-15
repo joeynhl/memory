fa588f0f4b0c49ff136728d8408190e5d0815a9e
Rienk <rienk2001@hotmail.com>
2019-10-15 12:44:10 +0200

    Rienk files toegevoegd

---
+++ memoryMain/Startscherm/Speeldveld-kiezen.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Startscherm
+{
+    /// <summary>
+    /// Interaction logic for Speeldveld_kiezen.xaml
+    /// </summary>
+    public partial class Speeldveld_kiezen : Window
+    {
+        public Speeldveld_kiezen()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
