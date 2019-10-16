ad51a53efe13e3eb1f14fd6592203b072a57a17d
qlimax89 <Kevin_25jan@hotmail.com>
2019-10-16 11:48:46 +0200

    merge

--- Startscherm/Startscherm/spelen.xaml.cs
+++ Startscherm/Startscherm/spelen.xaml.cs
@@ -27,26 +27,23 @@ namespace Startscherm
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-<<<<<<< HEAD
             String Speler1 = Textbox1_speler1.Text;
             String Speler2 = Textbox2_speler2.Text;
             namen namen = new namen();
 
             namen.Speler1 = Speler1;
             namen.Speler2 = Speler2;
-=======
             string naam1 = _1eNaam.Text;
             string naam2 = _2eNaam.Text;
+
             namen namen = new namen();
 
             namen.naam1 = naam1;
             namen.naam2 = naam2;
->>>>>>> e02bd7a7d91b0a401fa8e9433b54acd053c3e1e3
 
             this.Hide();
             namen.Show();
             this.Close();
-<<<<<<< HEAD
 
 
             //string naam1 = _1eNaam.Text;
@@ -59,8 +56,6 @@ namespace Startscherm
             //this.Hide();
             //namen.Show();
             //this.Close();
-=======
->>>>>>> e02bd7a7d91b0a401fa8e9433b54acd053c3e1e3
         }
 
     }
