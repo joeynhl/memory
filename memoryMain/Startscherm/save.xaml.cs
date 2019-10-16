f0e4fdcf739c40b987ff79f9a8bb9f56bb4f5361
stylishpillow <eelco_hansma@hotmail.nl>
2019-10-16 19:23:25 +0200

    added save class

---
+++ memoryMain/Startscherm/save.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace WpfTutorialSamples.Dialogs
+{
+    public partial class SaveFileDialogSample : Window
+    {
+        public SaveFileDialogSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+        }
+    }
+}
